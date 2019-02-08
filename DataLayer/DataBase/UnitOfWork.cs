5c66e97c1709631180e14bc2230edc9fc3f81cc8
Mateo Elizalde <mateo427499@hotmail.com>
2019-02-08 11:22:23 -0300

    Añadidos DetalleRutina y FichaMedica a Unit

--- DataLayer/DataBase/UnitOfWork.cs
+++ DataLayer/DataBase/UnitOfWork.cs
@@ -26,6 +26,7 @@ namespace DataLayer.DataBase
 
             this.SocioRepository = new SocioRepository(this.iDbContext);
             this.CuotaRepository = new CuotaRepository(this.iDbContext);
+            this.DetalleRutinaRepository = new DetalleRutinaRepository(this.iDbContext);
             this.EjercicioRepository = new EjercicioRepository(this.iDbContext);
             this.MusculoRepository = new MusculoRepository(this.iDbContext);
             //this.PagoRepository = new PagoRepository(this.iDbContext);
@@ -33,21 +34,16 @@ namespace DataLayer.DataBase
             //this.MovimientoRepository = new MovimientoRepository(this.iDbContext);
             //this.GastoAdministrativoRepository = new GastoAdministrativoRepository(this.iDbContext);
             this.ProductoRepository = new ProductoRepository(this.iDbContext);
-            //this.HistorialMedicoRepository = new HistorialMedicoRepository(this.iDbContext);
+            this.FichaMedicaRepository = new FichaMedicaRepository(this.iDbContext);
             //this.MarcaRepository = new MarcaRepository(this.iDbContext);
             this.TipoProductoRepository = new TipoProductoRepository(this.iDbContext);
         }
 
-        public ISocioRepository SocioRepository
-        {
-            get; private set;
-        }
+        public ISocioRepository SocioRepository { get; private set; }
 
-        public ICuotaRepository CuotaRepository
-        {
-            get; private set;
-        }
+        public ICuotaRepository CuotaRepository { get; private set; }
 
+        public IDetalleRutinaRepository DetalleRutinaRepository { get; private set; }
         public IEjercicioRepository EjercicioRepository { get; private set; }
         public IMusculoRepository MusculoRepository { get; private set; }
         //public IPagoRepository PagoRepository
@@ -75,10 +71,10 @@ namespace DataLayer.DataBase
         //public IMarcaRepository MarcaRepository { get; private set; }
         public ITipoProductoRepository TipoProductoRepository { get; private set; }
 
-        //public IHistorialMedicoRepository HistorialMedicoRepository
-        //{
-        //    get; private set;
-        //}
+        public IFichaMedicaRepository FichaMedicaRepository
+        {
+            get; private set;
+        }
 
         public void Complete()
         {

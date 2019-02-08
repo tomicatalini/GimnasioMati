6fcee67272bda12a166bf51f30155625ecd56479
Mateo Elizalde <mateo427499@hotmail.com>
2019-02-08 11:19:30 -0300

    Creados Repo e IRepo de DetalleRutina

---
+++ DataLayer/DataBase/DetalleRutinaRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer;
+using DataLayer.InterfacesRepositorio;
+
+namespace DataLayer.DataBase
+{
+    class DetalleRutinaRepository : Repository<DetalleRutina, GymMatiContext>, IDetalleRutinaRepository
+    {
+        public DetalleRutinaRepository(GymMatiContext pContext) : base(pContext) { }
+    }
+}
